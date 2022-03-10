4b16578f5f9bb2cb8152435b035f68eaf78d73c0
LiangYang666 <mail.liangyang@gmail.com>
2022-03-10 19:16:28 +0800

    推理脚本优化

--- run_inference.sh
+++ run_inference.sh
@@ -2,4 +2,4 @@
 
 PYTHONPATH="$(dirname $0)/../../":$PYTHONPATH
 
-python ./tools/detection/inference.py --input ./data/xyb/images/val --output ./results_show
+python ./tools/detection/inference.py -input ./data/xyb/images/val -output ./results_show
