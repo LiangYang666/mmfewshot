06c962c20778f2e7aa3545306e73ad711f2b4479
LiangYang666 <mail.liangyang@gmail.com>
2022-04-07 22:54:36 +0800

    完善移植平台v0.3交差

--- run_test.sh
+++ run_test.sh
@@ -6,11 +6,5 @@ PYTHONPATH="$(dirname $0)/../../":$PYTHONPATH
 python ./tools/detection/test.py \
 mytools/xyb_vis-rcnn_r50_c4_8xb4_novel-fine-tuning.py \
 checkpoints/xyb-fine-tuning-iter_2000.pth \
---cfg-options \
-data.samples_per_gpu=12 data.workers_per_gpu=4  \
-data.model_init.samples_per_gpu=12 data.model_init.workers_per_gpu=4 \
-evaluation.jsonfile_prefix=result/result \
---show-dir result/result_show \
---out result/result.pkl \
---eval bbox \
---show-score-thr 0.5
+-output ./result_show \
+--show-score-thr 0.4
