07d8d59318a147028ca3b7394f379317b6d69205
LiangYang666 <mail.liangyang@gmail.com>
2022-04-29 20:19:00 +0800

    更改map 计算虚警率

--- mytools/map.py
+++ mytools/map.py
@@ -12,6 +12,7 @@ import numpy as np
 
 def cac_map(dataset_dir: str):
     MINOVERLAP = 0.4
+    MIN_CONFIDENCE = 0.3
     no_animation = False
     no_plot = False
     quiet = False
@@ -368,6 +369,82 @@ def cac_map(dataset_dir: str):
         # close the plot
         plt.close()
 
+    def draw_plot_falsealarm(dictionary, n_classes, window_title, plot_title, x_label, output_path, to_show, plot_color, true_p_bar):
+        # sort the dictionary by decreasing value, into a list of tuples
+        sorted_dic_by_value = sorted(dictionary.items(), key=operator.itemgetter(1))
+        # unpacking the list of tuples into two lists
+        sorted_keys, sorted_values = zip(*sorted_dic_by_value)
+        #
+        """
+         Special case to draw in:
+            - green -> TP: True Positives (object detected and matches ground-truth)
+            - red -> FP: False Positives (object detected but does not match ground-truth)
+            # - pink -> FN: False Negatives (object not detected but present in the ground-truth)
+        """
+        fp_sorted = []
+        tp_sorted = []
+        for key in sorted_keys:
+            fp_sorted.append(dictionary[key] - true_p_bar[key])
+            tp_sorted.append(true_p_bar[key])
+        plt.barh(range(n_classes), tp_sorted, align='center', color='forestgreen', label='True Positive')
+        plt.barh(range(n_classes), fp_sorted, align='center', color='crimson', label='False Positive', left=tp_sorted)
+        # add legend
+        plt.legend(loc='lower right')
+        """
+         Write number on side of bar
+        """
+        fig = plt.gcf() # gcf - get current figure
+        axes = plt.gca()
+        r = fig.canvas.get_renderer()
+        for i, val in enumerate(sorted_values):
+            fp_val = fp_sorted[i]
+            tp_val = tp_sorted[i]
+            false_alarm = fp_val/(fp_val+tp_val)*100
+            tp_str_val = " " + str(tp_val)
+            fp_str_val = tp_str_val + " " + str(fp_val) + "  {:.2f}%".format(false_alarm)
+            # trick to paint multicolor with offset:
+            # first paint everything and then repaint the first number
+            t = plt.text(val, i, fp_str_val, color='crimson', va='center', fontweight='bold')
+            plt.text(val, i, tp_str_val, color='forestgreen', va='center', fontweight='bold')
+            if i == (len(sorted_values)-1): # largest bar
+                adjust_axes(r, t, fig, axes)
+
+        # set window title
+        fig.canvas.set_window_title(window_title)
+        # write classes in y axis
+        tick_font_size = 12
+        plt.yticks(range(n_classes), sorted_keys, fontsize=tick_font_size)
+        """
+         Re-scale height accordingly
+        """
+        init_height = fig.get_figheight()
+        # comput the matrix height in points and inches
+        dpi = fig.dpi
+        height_pt = n_classes * (tick_font_size * 1.4) # 1.4 (some spacing)
+        height_in = height_pt / dpi
+        # compute the required figure height
+        top_margin = 0.15 # in percentage of the figure height
+        bottom_margin = 0.05 # in percentage of the figure height
+        figure_height = height_in / (1 - top_margin - bottom_margin)
+        # set new height
+        if figure_height > init_height:
+            fig.set_figheight(figure_height)
+
+        # set plot title
+        plt.title(plot_title, fontsize=14)
+        # set axis titles
+        # plt.xlabel('classes')
+        plt.xlabel(x_label, fontsize='large')
+        # adjust size of window
+        fig.tight_layout()
+        # save the plot
+        fig.savefig(output_path)
+        # show image
+        if to_show:
+            plt.show()
+        # close the plot
+        plt.close()
+
     """
      Create a ".temp_files/" and "output/" directory
     """
@@ -524,7 +601,8 @@ def cac_map(dataset_dir: str):
                 if tmp_class_name == class_name:
                     #print("match")
                     bbox = left + " " + top + " " + right + " " + bottom
-                    bounding_boxes.append({"confidence": confidence, "file_id": file_id, "bbox": bbox})
+                    if float(confidence)>MIN_CONFIDENCE:
+                        bounding_boxes.append({"confidence": confidence, "file_id": file_id, "bbox": bbox})
                     #print(bounding_boxes)
         # sort detection-results by decreasing confidence
         bounding_boxes.sort(key=lambda x: float(x['confidence']), reverse=True)
@@ -541,6 +619,7 @@ def cac_map(dataset_dir: str):
     with open(output_files_path + "/output.txt", 'w') as output_file:
         output_file.write("# AP and precision/recall per class\n")
         count_true_positives = {}
+        det_counter_per_class = {}
         for class_index, class_name in enumerate(gt_classes):
             count_true_positives[class_name] = 0
             """
@@ -549,12 +628,17 @@ def cac_map(dataset_dir: str):
             dr_file = TEMP_FILES_PATH + "/" + class_name + "_dr.json"
             with open(dr_file, 'r') as f:
                 dr_data = json.load(f)
+            temp_gt_count = gt_counter_per_class[class_name]
+            print("pre", temp_gt_count)
 
             """
              Assign detection-results to ground-truth objects
             """
             nd = len(dr_data)
+            det_counter_per_class[class_name] = nd
             tp = [0] * nd # creates an array of zeros of size nd
+            tp_repeat = [0]*nd
+            fp_repeat = [0] * nd
             fp = [0] * nd
             for idx, detection in enumerate(dr_data):
                 file_id = detection["file_id"]
@@ -615,26 +699,31 @@ def cac_map(dataset_dir: str):
                     if class_name in specific_iou_classes:
                         index = specific_iou_classes.index(class_name)
                         min_overlap = float(iou_list[index])
+                # min_overlap = 0.2
                 if ovmax >= min_overlap:
                     if "difficult" not in gt_match:
-                            if not bool(gt_match["used"]):
-                                # true positive
-                                tp[idx] = 1
-                                gt_match["used"] = True
-                                count_true_positives[class_name] += 1
-                                # update the ".json" file
-                                with open(gt_file, 'w') as f:
-                                    f.write(json.dumps(ground_truth_data))
-                                if show_animation:
-                                    status = "MATCH!"
-                            else:
-                                # false positive (multiple detection)
-                                fp[idx] = 1
-                                if show_animation:
-                                    status = "REPEATED MATCH!"
+                        tp_repeat[idx] = 1
+                        if bool(gt_match["used"]):
+                            temp_gt_count += 1
+                        if not bool(gt_match["used"]):
+                            # true positive
+                            tp[idx] = 1
+                            gt_match["used"] = True
+                            count_true_positives[class_name] += 1
+                            # update the ".json" file
+                            with open(gt_file, 'w') as f:
+                                f.write(json.dumps(ground_truth_data))
+                            if show_animation:
+                                status = "MATCH!"
+                        else:
+                            # false positive (multiple detection)
+                            fp[idx] = 1
+                            if show_animation:
+                                status = "REPEATED MATCH!"
                 else:
                     # false positive
                     fp[idx] = 1
+                    fp_repeat[idx] = 1
                     if ovmax > 0:
                         status = "INSUFFICIENT OVERLAP"
 
@@ -692,6 +781,16 @@ def cac_map(dataset_dir: str):
                     cv2.imwrite(output_img_path, img)
                     # save the image with all the objects drawn to it
                     cv2.imwrite(img_cumulative_path, img_cumulative)
+            print(class_name)
+            # print("temp_gt_count", temp_gt_count)
+            # print("tp_repeat sum", sum(tp_repeat))
+            # print("fp_repeat sum", sum(fp_repeat))
+            print("det_counter_per_class", det_counter_per_class[class_name])
+            print("count_true_positives", count_true_positives[class_name])
+            print("tp sum", sum(tp))
+            print("fp sum", sum(fp))
+            print("tp", tp)
+            print("fp", fp)
 
             #print(tp)
             # compute precision/recall
@@ -707,6 +806,7 @@ def cac_map(dataset_dir: str):
             rec = tp[:]
             for idx, val in enumerate(tp):
                 rec[idx] = float(tp[idx]) / gt_counter_per_class[class_name]
+                # rec[idx] = float(tp[idx]) / temp_gt_count
             #print(rec)
             prec = tp[:]
             for idx, val in enumerate(tp):
@@ -800,23 +900,6 @@ def cac_map(dataset_dir: str):
     """
      Count total of detection-results
     """
-    # iterate through all the files
-    det_counter_per_class = {}
-    for txt_file in dr_files_list:
-        # get lines to list
-        lines_list = file_lines_to_list(txt_file)
-        for line in lines_list:
-            class_name = line.split()[0]
-            # check if class is in the ignore list, if yes skip
-            if class_name in ignore:
-                continue
-            # count that object
-            if class_name in det_counter_per_class:
-                det_counter_per_class[class_name] += 1
-            else:
-                # if class didn't exist yet
-                det_counter_per_class[class_name] = 1
-    #print(det_counter_per_class)
     dr_classes = list(det_counter_per_class.keys())
 
 
@@ -877,6 +960,7 @@ def cac_map(dataset_dir: str):
         to_show = False
         plot_color = 'forestgreen'
         true_p_bar = count_true_positives
+        print()
         draw_plot_func(
             # gt_counter_per_class,
             det_counter_per_class,
@@ -954,7 +1038,7 @@ def cac_map(dataset_dir: str):
         print(text)
 
     if draw_plot:
-        window_title = "detection-results-info"
+        window_title = "detection-results-info-recall"
         # Plot title
         plot_title = "detection-results\n"
         plot_title += "(" + str(len(dr_files_list)) + " files and "
@@ -962,13 +1046,13 @@ def cac_map(dataset_dir: str):
         plot_title += str(count_non_zero_values_in_dictionary) + " detected classes)"
         # end Plot title
         x_label = "Number of objects per class"
-        output_path = output_files_path + "/detection-results-info.png"
+        output_path = output_files_path + "/detection-results-info-recall.png"
         to_show = False
         plot_color = 'forestgreen'
         true_p_bar = count_true_positives
         draw_plot_func(
             gt_counter_per_class,
-            len(det_counter_per_class),
+            len(gt_counter_per_class),
             window_title,
             plot_title,
             x_label,
@@ -978,4 +1062,32 @@ def cac_map(dataset_dir: str):
             true_p_bar,
             flag=False
             )
+
+    if draw_plot:
+        window_title = "detection-results-info-false-alarm"
+        # Plot title
+        plot_title = "detection-results-false-alarm\n"
+        plot_title += "(" + str(len(dr_files_list)) + " files and "
+        count_non_zero_values_in_dictionary = sum(int(x) > 0 for x in list(det_counter_per_class.values()))
+        plot_title += str(count_non_zero_values_in_dictionary) + " detected classes)"
+        # end Plot title
+        x_label = "Number of objects per class"
+        output_path = output_files_path + "/detection-results-info-false-alarm.png"
+        to_show = False
+        plot_color = 'forestgreen'
+        true_p_bar = count_true_positives
+        draw_plot_falsealarm(
+            det_counter_per_class,
+            len(det_counter_per_class),
+            window_title,
+            plot_title,
+            x_label,
+            output_path,
+            to_show,
+            plot_color,
+            true_p_bar,
+            )
     return gt_counter_per_class, count_true_positives
+
+if __name__ =="__main__":
+    cac_map("/media/E_4TB/YL/mmlab/mmfewshot/result_show_inf/map_dir")
