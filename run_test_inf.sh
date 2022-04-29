426f91cb08c1d788d45509833d48f4436b4e92a6
LiangYang666 <mail.liangyang@gmail.com>
2022-04-29 20:28:51 +0800

    添加测试红外图

---
+++ run_test_inf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=0
+PYTHONPATH="$(dirname $0)/../../":$PYTHONPATH
+
+python ./tools/detection/test.py \
+mytools/xyb_inf-rcnn_r50_c4_8xb4_novel-fine-tuning.py \
+checkpoints/xyb_inf-fine-tuning-iter_2000.pth \
+-output ./result_show_inf \
+--show-score-thr 0.3 \
+--save-support-heatmap \
+--save-query-heatmap
